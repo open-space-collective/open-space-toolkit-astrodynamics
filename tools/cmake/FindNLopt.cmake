41da5a95c56ca391cf862fbdef1858f57289a84f
Lucas Brémond <lucas.bremond@gmail.com>
2018-09-10 22:20:11 -0700

    [fix] CMakeLists.txt

--- tools/cmake/FindNLopt.cmake
+++ tools/cmake/FindNLopt.cmake
@@ -1,37 +1,37 @@
-# Find NLOPT
+# Find NLopt
 
 # This sets the following variables:
-# NLOPT_FOUND
-# NLOPT_INCLUDE_DIRS
-# NLOPT_LIBRARIES
+# NLopt_FOUND
+# NLopt_INCLUDE_DIRS
+# NLopt_LIBRARIES
 # NLOPT_DEFINITIONS
 # NLOPT_VERSION
 
 find_package(PkgConfig QUIET)
 
 # Check to see if pkgconfig is installed.
-pkg_check_modules(PC_NLOPT nlopt QUIET)
+pkg_check_modules(PC_NLopt nlopt QUIET)
 
 # Definitions
-set(NLOPT_DEFINITIONS ${PC_NLOPT_CFLAGS_OTHER})
+set(NLopt_DEFINITIONS ${PC_NLopt_CFLAGS_OTHER})
 
 # Include directories
-find_path(NLOPT_INCLUDE_DIRS
+find_path(NLopt_INCLUDE_DIRS
     NAMES nlopt.h
-    HINTS ${PC_NLOPT_INCLUDEDIR}
+    HINTS ${PC_NLopt_INCLUDEDIR}
     PATHS "${CMAKE_INSTALL_PREFIX}/include")
 
 # Libraries
-find_library(NLOPT_LIBRARIES
+find_library(NLopt_LIBRARIES
     NAMES nlopt nlopt_cxx
-    HINTS ${PC_NLOPT_LIBDIR})
+    HINTS ${PC_NLopt_LIBDIR})
 
 # Version
-set(NLOPT_VERSION ${PC_NLOPT_VERSION})
+set(NLopt_VERSION ${PC_NLopt_VERSION})
 
 # Set (NAME)_FOUND if all the variables and the version are satisfied.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(NLOPT
+find_package_handle_standard_args(NLopt
     FAIL_MESSAGE  DEFAULT_MSG
-    REQUIRED_VARS NLOPT_INCLUDE_DIRS NLOPT_LIBRARIES
-    VERSION_VAR   NLOPT_VERSION)
+    REQUIRED_VARS NLopt_INCLUDE_DIRS NLopt_LIBRARIES
+    VERSION_VAR   NLopt_VERSION)
