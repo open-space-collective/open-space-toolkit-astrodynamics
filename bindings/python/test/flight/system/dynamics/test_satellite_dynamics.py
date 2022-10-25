2fe112e2a1f5f67908fa766694139278e8412e2c
Lucas Brémond <lucas@loftorbital.com>
2022-10-25 18:51:19 +0000

    refactor: ordering

--- bindings/python/test/flight/system/dynamics/test_satellite_dynamics.py
+++ bindings/python/test/flight/system/dynamics/test_satellite_dynamics.py
@@ -11,8 +11,9 @@ import pytest
 
 import numpy as np
 
-import ostk.physics as physics
 import ostk.mathematics as mathematics
+
+import ostk.physics as physics
 import ostk.astrodynamics as astrodynamics
 
 ################################################################################################################################################################
