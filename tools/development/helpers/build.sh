580359a3deed3a94272e81a9ae1fc17582122186
Antoine Paletta <antoine.paletta@loftorbital.com>
2023-07-20 21:59:13 +0000

    feat: improve build.sh helper by adding notest, nobind, and notestbind options

--- tools/development/helpers/build.sh
+++ tools/development/helpers/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Apache License 2.0 
+# Apache License 2.0
 
 project_directory="$(git rev-parse --show-toplevel)"
 build_directory="${project_directory}/build"
@@ -12,6 +12,18 @@ pushd "${build_directory}" > /dev/null
 if [[ ! -z $1 ]] && [[ $1 == "--debug" ]]; then
 
     cmake -DCMAKE_BUILD_TYPE=Debug ..
+
+elif [[ ! -z $1 ]] && [[ $1 == "--notest" ]]; then
+
+    cmake -DBUILD_UNIT_TESTS=OFF ..
+
+elif [[ ! -z $1 ]] && [[ $1 == "--nobind" ]]; then
+
+    cmake -DBUILD_PYTHON_BINDINGS=OFF ..
+
+elif [[ ! -z $1 ]] && [[ $1 == "--notestbind" ]]; then
+
+    cmake -DBUILD_PYTHON_BINDINGS=OFF -DBUILD_UNIT_TESTS=OFF ..
 
 else
 
