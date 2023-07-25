15ae06947cc1e31458c19141c3e6b86b9e601e90
vishwa shah <vishwa@loftorbital.com>
2023-07-25 22:47:15 +0000

    style: format

--- include/OpenSpaceToolkit/Astrodynamics/NumericalSolver.hpp
+++ include/OpenSpaceToolkit/Astrodynamics/NumericalSolver.hpp
@@ -228,8 +228,8 @@ class NumericalSolver
         const SystemOfEquationsWrapper& aSystemOfEquations
     );
 
-    /// @brief              Perform numerical integration from a start time until either a condition or the end time is met
-    /// reached
+    /// @brief              Perform numerical integration from a start time until either a condition or the end time is
+    /// met reached
     ///
     /// @code
     ///                     StateVector stateVector = numericalSolver.integrateTime(stateVector,
