f96a03409bfaec6c6e84862cddd1e893961bc6ab
Pau Hebrero <pau.hebrero@gmail.com>
2023-07-26 10:12:02 +0000

    feat: add Coordinates Broker and Subset

---
+++ src/OpenSpaceToolkit/Astrodynamics/Trajectory/CoordinatesSubset.cpp
@@ -0,0 +1,91 @@
+/// Apache License 2.0
+
+#include <OpenSpaceToolkit/Core/Error.hpp>
+
+#include <OpenSpaceToolkit/Astrodynamics/Trajectory/CoordinatesSubset.hpp>
+
+namespace ostk
+{
+namespace astro
+{
+namespace trajectory
+{
+
+CoordinatesSubset::CoordinatesSubset(const String& aName, const Size& aSize)
+    : name_(aName),
+      size_(aSize)
+{
+    if (aName.isEmpty())
+    {
+        throw ostk::core::error::runtime::Wrong("Name");
+    }
+
+    if (aSize == 0)
+    {
+        throw ostk::core::error::runtime::Wrong("Size");
+    }
+
+    this->id_ = aName + " | " + std::to_string(aSize);
+}
+
+bool CoordinatesSubset::operator==(const CoordinatesSubset& aCoordinatesSubset) const
+{
+    return this->id_ == aCoordinatesSubset.id_;
+}
+
+bool CoordinatesSubset::operator!=(const CoordinatesSubset& aCoordinatesSubset) const
+{
+    return !((*this) == aCoordinatesSubset);
+}
+
+String CoordinatesSubset::getId() const
+{
+    return id_;
+}
+
+String CoordinatesSubset::getName() const
+{
+    return name_;
+}
+
+Size CoordinatesSubset::getSize() const
+{
+    return size_;
+}
+
+VectorXd CoordinatesSubset::add(
+    [[maybe_unused]] const Instant& anInstant,
+    [[maybe_unused]] const VectorXd& allCoordinates_1,
+    [[maybe_unused]] const VectorXd& allCoordinates_2,
+    [[maybe_unused]] const Shared<const Frame>& aFrame,
+    [[maybe_unused]] const Shared<const CoordinatesBroker>& aCoordinatesBroker
+) const
+{
+    throw ostk::core::error::runtime::ToBeImplemented("Addition");
+}
+
+VectorXd CoordinatesSubset::subtract(
+    [[maybe_unused]] const Instant& anInstant,
+    [[maybe_unused]] const VectorXd& allCoordinates_1,
+    [[maybe_unused]] const VectorXd& allCoordinates_2,
+    [[maybe_unused]] const Shared<const Frame>& aFrame,
+    [[maybe_unused]] const Shared<const CoordinatesBroker>& aCoordinatesBroker
+) const
+{
+    throw ostk::core::error::runtime::ToBeImplemented("Subtraction");
+}
+
+VectorXd CoordinatesSubset::inFrame(
+    [[maybe_unused]] const Instant& anInstant,
+    [[maybe_unused]] const VectorXd& allCoordinates,
+    [[maybe_unused]] const Shared<const Frame>& fromFrame,
+    [[maybe_unused]] const Shared<const Frame>& toFrame,
+    [[maybe_unused]] const Shared<const CoordinatesBroker>& aCoordinatesBroker
+) const
+{
+    throw ostk::core::error::runtime::ToBeImplemented("Frame transformation");
+}
+
+}  // namespace trajectory
+}  // namespace astro
+}  // namespace ostk
---
+++ test/OpenSpaceToolkit/Astrodynamics/Trajectory/CoordinatesSubset.test.cpp
@@ -0,0 +1,89 @@
+/// Apache License 2.0
+
+#include <OpenSpaceToolkit/Astrodynamics/Trajectory/CoordinatesSubset.hpp>
+
+#include <Global.test.hpp>
+
+using ostk::core::types::Shared;
+using ostk::core::types::String;
+
+using ostk::math::obj::VectorXd;
+
+using ostk::physics::coord::Frame;
+using ostk::physics::time::Instant;
+
+using ostk::astro::trajectory::CoordinatesBroker;
+using ostk::astro::trajectory::CoordinatesSubset;
+
+TEST(OpenSpaceToolkit_Astrodynamics_Trajectory_CoordinatesSubset, Constructor)
+{
+    {
+        EXPECT_NO_THROW(CoordinatesSubset("NAME", 1));
+    }
+
+    {
+        EXPECT_ANY_THROW(CoordinatesSubset("", 0));
+        EXPECT_ANY_THROW(CoordinatesSubset("NAME", 0));
+    }
+}
+
+TEST(OpenSpaceToolkit_Astrodynamics_Trajectory_CoordinatesSubset, EqualToOperator)
+{
+    {
+        EXPECT_TRUE(CoordinatesSubset("NAME", 1) == CoordinatesSubset("NAME", 1));
+    }
+
+    {
+        EXPECT_FALSE(CoordinatesSubset("NAME", 1) == CoordinatesSubset("OTHER", 1));
+        EXPECT_FALSE(CoordinatesSubset("NAME", 1) == CoordinatesSubset("NAME", 2));
+    }
+}
+
+TEST(OpenSpaceToolkit_Astrodynamics_Trajectory_CoordinatesSubset, NotEqualToOperator)
+{
+    {
+        EXPECT_FALSE(CoordinatesSubset("NAME", 1) != CoordinatesSubset("NAME", 1));
+    }
+
+    {
+        EXPECT_TRUE(CoordinatesSubset("NAME", 1) != CoordinatesSubset("OTHER", 1));
+        EXPECT_TRUE(CoordinatesSubset("NAME", 1) != CoordinatesSubset("NAME", 2));
+    }
+}
+
+TEST(OpenSpaceToolkit_Astrodynamics_Trajectory_CoordinatesSubset, Getters)
+{
+    {
+        const CoordinatesSubset coordinatesSubset = CoordinatesSubset("NAME", 1);
+
+        EXPECT_EQ("NAME", coordinatesSubset.getName());
+        EXPECT_EQ(1, coordinatesSubset.getSize());
+    }
+}
+
+TEST(OpenSpaceToolkit_Astrodynamics_Trajectory_CoordinatesSubset, Add)
+{
+    {
+        const CoordinatesSubset coordinatesSubset = CoordinatesSubset("NAME", 1);
+
+        EXPECT_ANY_THROW(coordinatesSubset.add(Instant::Undefined(), VectorXd(0), VectorXd(0), nullptr, nullptr));
+    }
+}
+
+TEST(OpenSpaceToolkit_Astrodynamics_Trajectory_CoordinatesSubset, Subtract)
+{
+    {
+        const CoordinatesSubset coordinatesSubset = CoordinatesSubset("NAME", 1);
+
+        EXPECT_ANY_THROW(coordinatesSubset.add(Instant::Undefined(), VectorXd(0), VectorXd(0), nullptr, nullptr));
+    }
+}
+
+TEST(OpenSpaceToolkit_Astrodynamics_Trajectory_CoordinatesSubset, InFrame)
+{
+    {
+        const CoordinatesSubset coordinatesSubset = CoordinatesSubset("NAME", 1);
+
+        EXPECT_ANY_THROW(coordinatesSubset.inFrame(Instant::Undefined(), VectorXd(0), nullptr, nullptr, nullptr));
+    }
+}
