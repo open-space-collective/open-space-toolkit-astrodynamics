24a2fceaca571c043ca35e64ecc4e8dde42cd620
Pau Hebrero <pau.hebrero@gmail.com>
2023-08-09 15:47:37 +0000

    feat: wip benchmark

--- test/OpenSpaceToolkit/Astrodynamics/Benchmark/scenarios/scenario001.hpp
+++ test/OpenSpaceToolkit/Astrodynamics/Benchmark/scenarios/scenario001.hpp
@@ -5,7 +5,7 @@ int scenario001(int const i) {
   if (i <= 1) {
     return 1;
   }
-  return scenario001(i - 2) + scenario001(i - 1);
+  return scenario001(i - 2) + scenario001(i - 1) + scenario001(i-3);
 }
 
 #endif // SCENARIO001_HPP_INCLUDED
--- test/OpenSpaceToolkit/Astrodynamics/Benchmark/scenarios/scenario002.hpp
+++ test/OpenSpaceToolkit/Astrodynamics/Benchmark/scenarios/scenario002.hpp
@@ -5,7 +5,7 @@ int scenario002(int const i) {
   if (i <= 1) {
     return 1;
   }
-  return scenario002(i - 2) + scenario002(i - 1);
+  return scenario002(i - 2) + scenario002(i - 1) + scenario002(i-3);
 }
 
 #endif // SCENARIO002_HPP_INCLUDED
