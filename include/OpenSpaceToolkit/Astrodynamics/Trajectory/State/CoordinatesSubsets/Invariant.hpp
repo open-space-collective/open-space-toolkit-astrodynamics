cd92b4f38df581295e930d9f5b9a4699e6c59377
Remy Derollez <remy@loftorbital.com>
2023-08-23 14:31:08 +0000

    feat: add Invariant coordinatesSubset

---
+++ include/OpenSpaceToolkit/Astrodynamics/Trajectory/State/CoordinatesSubsets/Invariant.hpp
@@ -0,0 +1,110 @@
+/// Apache License 2.0
+
+#ifndef __OpenSpaceToolkit_Astrodynamics_Trajectory_State_CoordinatesSubsets_Invariant__
+#define __OpenSpaceToolkit_Astrodynamics_Trajectory_State_CoordinatesSubsets_Invariant__
+
+#include <OpenSpaceToolkit/Astrodynamics/Trajectory/State/CoordinatesBroker.hpp>
+
+namespace ostk
+{
+namespace astro
+{
+namespace trajectory
+{
+namespace state
+{
+namespace coordinatessubsets
+{
+
+using ostk::core::types::Shared;
+using ostk::core::types::String;
+
+using ostk::physics::time::Instant;
+
+using ostk::astro::trajectory::state::CoordinatesSubset;
+using ostk::astro::trajectory::state::CoordinatesBroker;
+
+/// @brief  Cartesian Position.
+
+class Invariant : public CoordinatesSubset
+{
+   public:
+    /// @brief              Constructor
+    ///
+    /// @param              [in] aName a name
+    /// @param              [in] aSize a size
+
+    Invariant(const String& aName, const Size& aSize);
+
+    /// @brief              Destructor
+
+    ~Invariant();
+
+    /// @brief              Adds two coordinates subsets
+    ///
+    /// @param              [in] anInstant the instant associated to the coordinates
+    /// @param              [in] aFullCoordinatesVector first set of all coordinates
+    /// @param              [in] anotherFullCoordinatesVector second set of all coordinates
+    /// @param              [in] aFrameSPtr the reference frame associated to the coordinates
+    /// @param              [in] aCoordinatesBrokerSPtr a coordinates broker
+    ///
+    /// @return             The resulting coordinates subset value (aSubset + anotherSubset)
+
+    VectorXd add(
+        const Instant& anInstant,
+        const VectorXd& aFullCoordinatesVector,
+        const VectorXd& anotherFullCoordinatesVector,
+        const Shared<const Frame>& aFrameSPtr,
+        const Shared<const CoordinatesBroker>& aCoordinatesBrokerSPtr
+    ) const override;
+
+    /// @brief              Subtracts two coordinates subsets
+    ///
+    /// @param              [in] anInstant the instant associated to the coordinates
+    /// @param              [in] aFullCoordinatesVector first set of all coordinates
+    /// @param              [in] anotherFullCoordinatesVector second set of all coordinates
+    /// @param              [in] aFrameSPtr the reference frame associated to the coordinates
+    /// @param              [in] aCoordinatesBrokerSPtr a coordinates broker
+    ///
+    /// @return             The resulting coordinates subset value (aSubset - anotherSubset)
+
+    VectorXd subtract(
+        const Instant& anInstant,
+        const VectorXd& aFullCoordinatesVector,
+        const VectorXd& anotherFullCoordinatesVector,
+        const Shared<const Frame>& aFrameSPtr,
+        const Shared<const CoordinatesBroker>& aCoordinatesBrokerSPtr
+    ) const override;
+
+    /// @brief              Transforms the coordinate subset from one frame to another
+    ///
+    /// @param              [in] anInstant the reference frame associated to the coordinates
+    /// @param              [in] aFullCoordinatesVector all coordinates
+    /// @param              [in] fromFrame the reference frame associated to the coordinates
+    /// @param              [in] toFrame the reference frame in which the coordinates are to be transformed
+    /// @param              [in] aCoordinatesBrokerSPtr a coordinates broker
+    ///
+    /// @return             The resulting coordinates subset value expressed in the desired reference frame
+
+    VectorXd inFrame(
+        const Instant& anInstant,
+        const VectorXd& aFullCoordinatesVector,
+        const Shared<const Frame>& fromFrame,
+        const Shared<const Frame>& toFrame,
+        const Shared<const CoordinatesBroker>& aCoordinatesBrokerSPtr
+    ) const override;
+
+    /// @brief              Return the default instance
+    ///
+    /// @return             The default instance
+
+    static Shared<const Invariant> Mass();
+};
+
+}  // namespace coordinatessubsets
+}  // namespace state
+}  // namespace trajectory
+}  // namespace astro
+}  // namespace ostk
+
+#endif
