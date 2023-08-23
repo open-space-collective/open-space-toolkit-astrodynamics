076354e16f0da3b60dd72f47bd6cb70c20cc8001
Remy Derollez <remy@loftorbital.com>
2023-08-23 15:21:26 +0000

    feat: add dummy local orbital frame and provider classes

---
+++ src/OpenSpaceToolkit/Astrodynamics/Trajectory/LocalOrbitalProvider.cpp
@@ -0,0 +1,56 @@
+/// Apache License 2.0
+
+#include <OpenSpaceToolkit/Core/Error.hpp>
+#include <OpenSpaceToolkit/Core/Utilities.hpp>
+
+#include <OpenSpaceToolkit/Astrodynamics/Trajectory/LocalOrbitalProvider.hpp>
+
+namespace ostk
+{
+namespace astro
+{
+namespace trajectory
+{
+
+LOFProvider::LOFProvider(const LOFProvider::Generator& aGenerator)
+    : generator_(aGenerator)
+{
+}
+
+LOFProvider::~LOFProvider() {}
+
+LOFProvider* LOFProvider::clone() const
+{
+    return new LOFProvider(*this);
+}
+
+bool LOFProvider::isDefined() const
+{
+    return !!generator_;
+}
+
+Transform LOFProvider::getTransformAt(const Instant& anInstant) const
+{
+    (void)(anInstant);
+
+    // TBI: Not clean
+}
+
+Transform LOFProvider::getTransformAt(const Instant& anInstant, const Position& aPosition, const Velocity& aVelocity) const
+{
+    if (!this->isDefined())
+    {
+        throw ostk::core::error::runtime::Undefined("LOFProvider provider");
+    }
+
+    return generator_(anInstant, aPosition, aVelocity);
+}
+
+LOFProvider LOFProvider::Undefined()
+{
+    return {{}};
+}
+
+}  // namespace trajectory
+}  // namespace astro
+}  // namespace ostk
