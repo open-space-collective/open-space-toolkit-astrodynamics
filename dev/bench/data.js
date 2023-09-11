074e22d2554518238a1425b917bb81f29ec8526d
github-action-benchmark <github@users.noreply.github.com>
2023-09-11 23:39:39 +0000

    add Benchmark (googlecpp) benchmark result for 77fa2aaf8e3681d2f6f4bcc915a36d3a606ecdf2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1694021857761,
+  "lastUpdate": 1694475579773,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -108,6 +108,60 @@ window.BENCHMARK_DATA = {
             "value": 5488055923.7999935,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 5486909909.999997 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vishwa2710@gmail.com",
+            "name": "Vishwa Shah",
+            "username": "vishwa2710"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "77fa2aaf8e3681d2f6f4bcc915a36d3a606ecdf2",
+          "message": "fix: fix angle delta calculations in Event Conditions (#220)\n\n* fix: fix angle delta calculations in Event Conditions\r\n\r\n* feat: first cut at Angular Condition\r\n\r\n* feat: fix tests\r\n\r\n* feat: style and tests\r\n\r\n* feat: fix remaining tests\r\n\r\n* feat: fix tests for real\r\n\r\n* feat: address remaining feedback",
+          "timestamp": "2023-09-11T23:04:05Z",
+          "tree_id": "bc3e2b5ea8ca7f04ae790aa205373d5e172bad17",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/77fa2aaf8e3681d2f6f4bcc915a36d3a606ecdf2"
+        },
+        "date": 1694475578944,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 682143666.6000011,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 682079029.9999998 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 1443793178.6999967,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 1443678679.9999995 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 4147754647.900007,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 4147516989.999999 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 4148592181.300006,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 4147987380.0000024 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 4148933720.400004,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 4148031509.999994 ns\nthreads: 1"
           }
         ]
       }
