b89e082402180274cb1d8e27ae6d858abb8b09f6
github-action-benchmark <github@users.noreply.github.com>
2023-10-10 23:25:11 +0000

    add Benchmark (googlecpp) benchmark result for 9b7810aac14a3d4053c92a04818fdbfa2156ec70

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1696897383970,
+  "lastUpdate": 1696980311680,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -54,6 +54,60 @@ window.BENCHMARK_DATA = {
             "value": 4634613640.899994,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 4633127270 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kyle.ray.cochran@gmail.com",
+            "name": "kyle-cochran",
+            "username": "kyle-cochran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9b7810aac14a3d4053c92a04818fdbfa2156ec70",
+          "message": "refactor: Use the built-in State method to extract coordinates (#235)\n\n* chore: rebase conflicts\r\n\r\n* feat: add bindings (and fix typo bug\r\n\r\n* refactor: leverage State subsets extraction to simplify\r\n\r\n* chore: remove artifact from rebase",
+          "timestamp": "2023-10-10T15:50:49-07:00",
+          "tree_id": "e6a286dabee708f95a0725fbfb5d051e11dcdad2",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/9b7810aac14a3d4053c92a04818fdbfa2156ec70"
+        },
+        "date": 1696980310547,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 735488509.0000039,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 735248220.0000001 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 1651946364.4000096,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 1651804490 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 4111785805.599993,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 4110832650.000001 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 4114813365.399999,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 4114536540.0000014 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 4106929186.799999,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 4106630000.0000014 ns\nthreads: 1"
           }
         ]
       }
