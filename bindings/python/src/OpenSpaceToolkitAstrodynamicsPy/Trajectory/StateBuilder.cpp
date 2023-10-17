901d00521c431105781cf86a6d54743812d50e80
Vishwa Shah <vishwa2710@gmail.com>
2023-10-17 16:44:19 -0700

    feat: add missing args for StateBuilder bindings (#255)
    * feat: add missing bindings for StateBuilder
    
    * Apply suggestions from code review
    
    Co-authored-by: kyle-cochran <kyle.ray.cochran@gmail.com>
    
    ---------
    
    Co-authored-by: kyle-cochran <kyle.ray.cochran@gmail.com>

--- bindings/python/src/OpenSpaceToolkitAstrodynamicsPy/Trajectory/StateBuilder.cpp
+++ bindings/python/src/OpenSpaceToolkitAstrodynamicsPy/Trajectory/StateBuilder.cpp
@@ -54,11 +54,12 @@ inline void OpenSpaceToolkitAstrodynamicsPy_Trajectory_StateBuilder(pybind11::mo
 
         .def("is_defined", &StateBuilder::isDefined)
 
-        .def("build", &StateBuilder::build)
-        .def("reduce", &StateBuilder::reduce)
-        .def("expand", &StateBuilder::expand)
+        .def("build", &StateBuilder::build, arg("instant"), arg("coordinates"))
+        .def("reduce", &StateBuilder::reduce, arg("state"))
+        .def("expand", &StateBuilder::expand, arg("state"), arg("default_state"))
 
         .def("get_coordinates_subsets", &StateBuilder::getCoordinatesSubsets)
+        .def("access_coordinates_broker", &StateBuilder::accessCoordinatesBroker)
         .def("get_frame", &StateBuilder::getFrame)
 
         .def_static("undefined", &StateBuilder::Undefined)
