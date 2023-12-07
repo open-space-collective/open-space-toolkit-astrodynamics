eb705b63e4a0baf328c3c45993497bc8854805fc
github-action-benchmark <github@users.noreply.github.com>
2023-12-07 11:13:15 +0000

    add Benchmark (googlecpp) benchmark result for 8dcd766727b734429da17a53443267dde32c6653

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701903269268,
+  "lastUpdate": 1701947595616,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -162,6 +162,60 @@ window.BENCHMARK_DATA = {
             "value": 2785493192.300021,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 2785250850.000003 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "98616558+apaletta3@users.noreply.github.com",
+            "name": "Antoine Paletta",
+            "username": "apaletta3"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8dcd766727b734429da17a53443267dde32c6653",
+          "message": "feat: add fixed step size factory method for numerical solver (#295)\n\nCo-authored-by: Vishwa Shah <vishwa2710@gmail.com>",
+          "timestamp": "2023-12-07T10:45:42Z",
+          "tree_id": "71530a97aa2e8e65730f3b90eac480a8ae0ff5d5",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/8dcd766727b734429da17a53443267dde32c6653"
+        },
+        "date": 1701947595112,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 557209670.900005,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 557083959.9999998 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 791441389.3000017,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 791408910.0000001 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 2787822612.8000107,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2787591730.000001 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 2782689544.799996,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2782578060 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 2781535883.799989,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2781499050.0000033 ns\nthreads: 1"
           }
         ]
       }
