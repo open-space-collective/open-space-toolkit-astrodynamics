88cecd72621e14b98072d3156a9f1b3e23bc07d7
github-action-benchmark <github@users.noreply.github.com>
2023-12-12 14:31:02 +0000

    add Benchmark (googlecpp) benchmark result for eedcef530a7726f8d45604745d20eb5ac4cbd269

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701947595616,
+  "lastUpdate": 1702391462734,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -216,6 +216,60 @@ window.BENCHMARK_DATA = {
             "value": 2781535883.799989,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 2781499050.0000033 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "65550121+phc1990@users.noreply.github.com",
+            "name": "Pau Hebrero",
+            "username": "phc1990"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "eedcef530a7726f8d45604745d20eb5ac4cbd269",
+          "message": "feat: add Satellite System Builder (#299)\n\n* feat: add Satellite System Builder\r\n\r\n---------\r\n\r\nCo-authored-by: Pau Hebrero <pau.hebrero@gmail.com>",
+          "timestamp": "2023-12-12T15:03:09+01:00",
+          "tree_id": "4cbd4cdcb12aeeaa8920bcf5c5aae12967dcb7c1",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/eedcef530a7726f8d45604745d20eb5ac4cbd269"
+        },
+        "date": 1702391461426,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 559453475.6999962,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 559416369.9999999 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 805787165.8000067,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 805624090.0000002 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 2785139060.7999916,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2785002470.000001 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 2783869402.800008,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2783708169.999999 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 2785007710.7000003,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2784880569.999997 ns\nthreads: 1"
           }
         ]
       }
