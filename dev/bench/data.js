99ec8aaad4b5176be437b9c8eb30723989987194
github-action-benchmark <github@users.noreply.github.com>
2024-01-04 16:36:34 +0000

    add Benchmark (googlecpp) benchmark result for 4daa7f949e20af5449552d0bb941502f24499d67

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1704295924684,
+  "lastUpdate": 1704386194642,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -216,6 +216,60 @@ window.BENCHMARK_DATA = {
             "value": 2788998041.0000024,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 2788917470.000001 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "65550121+phc1990@users.noreply.github.com",
+            "name": "Pau Hebrero",
+            "username": "phc1990"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4daa7f949e20af5449552d0bb941502f24499d67",
+          "message": "refactor: unify profile and trajectory States (#305)\n\n* feat: add Attitude Quaternion and Angular Velocity Coordinate Subsets\r\n\r\n* feat: add State pos/vel/attitude constructor\r\n\r\n* tests: add AttitudeQuaternion and AngularVelocity test\r\n\r\n* test: add attitude and angular velocity tests to State\r\n\r\n* fix: angular velocity subset size\r\n\r\n* feat: add Attitude Quaternion and Angular Velocity Coordinate Subsets\r\n\r\n* tests: add AttitudeQuaternion and AngularVelocity test\r\n\r\n* chore: apply suggestions\r\n\r\n* build: add trajectory state attitude-related bindings\r\n\r\n---------\r\n\r\nCo-authored-by: Pau Hebrero <pau.hebrero@gmail.com>",
+          "timestamp": "2024-01-04T17:08:20+01:00",
+          "tree_id": "b0076e764cbef41e1737d3ad4cad869c19c93fe3",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/4daa7f949e20af5449552d0bb941502f24499d67"
+        },
+        "date": 1704386193721,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 562413444.6000028,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 562400449.9999999 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 797908484.1999964,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 797865089.9999998 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 2789112848.700006,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2788950740.000001 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 2785000989.09999,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2784663600.000002 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 2784339099.9000063,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2784245500 ns\nthreads: 1"
           }
         ]
       }
