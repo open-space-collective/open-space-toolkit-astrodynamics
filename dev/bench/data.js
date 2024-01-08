ceece37d360047dbd1bc674abcac66cfb97565f0
github-action-benchmark <github@users.noreply.github.com>
2024-01-08 08:30:59 +0000

    add Benchmark (googlecpp) benchmark result for 785fbb5658efb900348572d85bb0be96676df3bc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1704402812725,
+  "lastUpdate": 1704702659807,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -324,6 +324,60 @@ window.BENCHMARK_DATA = {
             "value": 2783621738.699998,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 2783464380.000001 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vishwa2710@gmail.com",
+            "name": "Vishwa Shah",
+            "username": "vishwa2710"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "785fbb5658efb900348572d85bb0be96676df3bc",
+          "message": "chore: fix doxygen formatting (#289)\n\n* chore: fix doxygen formatting\r\n\r\n* Apply suggestions from code review\r\n\r\nCo-authored-by: Remy Derollez <43561754+Derollez@users.noreply.github.com>\r\n\r\n* Apply suggestions from code review\r\n\r\nCo-authored-by: Remy Derollez <43561754+Derollez@users.noreply.github.com>\r\n\r\n* docs: align sequence api docs with function definition (#311)\r\n\r\n* chore: fix doxygen formatting\r\n\r\n* feat: realign brief\r\n\r\n---------\r\n\r\nCo-authored-by: Remy Derollez <43561754+Derollez@users.noreply.github.com>\r\nCo-authored-by: Antoine Paletta <98616558+apaletta3@users.noreply.github.com>",
+          "timestamp": "2024-01-08T08:03:18Z",
+          "tree_id": "38326a8afce3c1e28ec3afcbcac98dca569f5088",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/785fbb5658efb900348572d85bb0be96676df3bc"
+        },
+        "date": 1704702659168,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 550600148.0999942,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 550573000 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 791771256.3999999,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 791722449.9999998 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 2820256563.800012,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2820193920.0000005 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 2815761913,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2815682739.9999976 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 2814991687.199995,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2814904490.0000024 ns\nthreads: 1"
           }
         ]
       }
