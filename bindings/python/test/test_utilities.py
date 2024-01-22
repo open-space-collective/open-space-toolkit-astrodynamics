9eae0db7077f7dc98bc6d0789138f4b8646c7705
vishwa shah <vishwa2710@gmail.com>
2024-01-22 21:00:41 +0000

    chore: fix tetsts

--- bindings/python/test/test_utilities.py
+++ bindings/python/test/test_utilities.py
@@ -23,8 +23,11 @@ State = astrodynamics.flight.profile.State
 
 
 class TestUtilities:
-    def test_lla_from_state(self, state: State):
-        lla: list = utilities.lla_from_state(state)
+    def test_lla_from_state(
+        self,
+        state: State,
+    ):
+        lla: tuple[float, float, float] = utilities.lla_from_state(state)
 
         assert lla is not None
         assert len(lla) == 3
@@ -47,13 +50,16 @@ class TestUtilities:
         assert isinstance(lla, LLA)
         assert lla.is_defined()
 
-        lla_from_state: list = utilities.lla_from_state(state)
+        lla_from_state: tuple[float, float, float] = utilities.lla_from_state(state)
 
         assert lla.get_latitude().in_degrees() == lla_from_state[0]
         assert lla.get_longitude().in_degrees() == lla_from_state[1]
         assert lla.get_altitude().in_meters() == lla_from_state[2]
 
-    def test_position_from_lla(self, lla: LLA):
+    def test_position_from_lla(
+        self,
+        lla: LLA,
+    ):
         position: Position = utilities.position_from_lla(lla)
 
         assert position is not None
@@ -61,20 +67,28 @@ class TestUtilities:
         assert position.is_defined()
 
     def test_compute_aer(
-        self, instant: Instant, position: Position, environment: Environment
+        self,
+        instant: Instant,
+        position: Position,
+        environment: Environment,
     ):
-        aer: list = utilities.compute_aer(instant, position, position, environment)
+        aer: tuple[float, float, float] = utilities.compute_aer(
+            instant, position, position, environment
+        )
 
         assert aer is not None
         assert len(aer) == 3
         assert aer[2] == 0.0
 
     def test_compute_time_lla_aer_state(
-        self, state: State, position: Position, environment: Environment
+        self,
+        state: State,
+        position: Position,
+        environment: Environment,
     ):
-        time_lla_aer: list = utilities.compute_time_lla_aer_state(
-            state, position, environment
-        )
+        time_lla_aer: float[
+            Instant, float, float, float, float, float, float
+        ] = utilities.compute_time_lla_aer_state(state, position, environment)
 
         assert time_lla_aer is not None
         assert len(time_lla_aer) == 7
@@ -84,15 +98,21 @@ class TestUtilities:
             assert isinstance(time_lla_aer[i], float)
 
     def test_compute_trajectory_geometry(
-        self, interval: Interval, trajectory: Trajectory
+        self,
+        interval: Interval,
+        trajectory: Trajectory,
     ):
-        output: list = utilities.compute_trajectory_geometry(trajectory, interval)
+        output: list[tuple[float, float, float]] = utilities.compute_trajectory_geometry(
+            trajectory, interval
+        )
 
         assert output is not None
         assert len(output) == 2
 
     def test_convert_state(self, instant: Instant, state: State):
-        output: list = utilities.convert_state(instant, state)
+        output: tuple[
+            str, float, float, float, float, float, float, float, float, float
+        ] = utilities.convert_state(instant, state)
 
         assert output is not None
         assert len(output) == 11
