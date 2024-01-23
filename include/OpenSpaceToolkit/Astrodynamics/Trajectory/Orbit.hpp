5f99c51bc5fc0da873c127c10babb2890b245a2e
vishwa shah <vishwa2710@gmail.com>
2024-01-23 08:46:24 +0000

    fix: update pass computation epoch to improve accuracy

--- include/OpenSpaceToolkit/Astrodynamics/Trajectory/Orbit.hpp
+++ include/OpenSpaceToolkit/Astrodynamics/Trajectory/Orbit.hpp
@@ -203,7 +203,10 @@ class Orbit : public Trajectory
     String generateFrameName(const Orbit::FrameType& aFrameType) const;
 
     static Instant GetCrossingInstant(
-        const Instant& previousInstant, const Instant& currentInstant, const std::function<double(double)>& getValue
+        const Instant& anEpoch,
+        const Instant& previousInstant,
+        const Instant& currentInstant,
+        const std::function<double(double)>& getValue
     );
 
     static Array<State> GenerateStates(const Model& aModel, const Array<Instant>& anInstantGrid);
