8aa059e714a7b7c7911f1f6b0d5c12fff0c369cf
kyle-cochran <kyle.ray.cochran@gmail.com>
2024-02-12 23:10:23 -0800

    Apply suggestions from code review
    self review

--- bindings/python/src/OpenSpaceToolkitAstrodynamicsPy/Trajectory/State/CoordinateSubset/AttitudeQuaternion.cpp
+++ bindings/python/src/OpenSpaceToolkitAstrodynamicsPy/Trajectory/State/CoordinateSubset/AttitudeQuaternion.cpp
@@ -53,7 +53,7 @@ inline void OpenSpaceToolkitAstrodynamicsPy_Trajectory_State_CoordinateSubset_At
                     coordinates (numpy.ndarray): The Attitude quaternion to convert.
                     from_frame (str): The reference frame of the input Attitude quaternion.
                     to_frame (str): The reference frame of the output Attitude quaternion.
-                    coordinate_broker (CoordinateBroker): The coordinates broker.
+                    coordinate_broker (CoordinateBroker): The coordinate broker.
 
                 Returns:
                     numpy.ndarray: The Attitude quaternion in the output reference frame.
