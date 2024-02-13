a0bc26d922e39d2338819df02d52b56ebd728fc0
github-action-benchmark <github@users.noreply.github.com>
2024-02-13 18:47:18 +0000

    add Benchmark (googlecpp) benchmark result for 6b65f18a98bf5697f742066c7b2a0dc649485a5d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707817307069,
+  "lastUpdate": 1707850037769,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -216,6 +216,60 @@ window.BENCHMARK_DATA = {
             "value": 2818194196.5999956,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 2818051550.000001 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kyle.ray.cochran@gmail.com",
+            "name": "kyle-cochran",
+            "username": "kyle-cochran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6b65f18a98bf5697f742066c7b2a0dc649485a5d",
+          "message": "refactor!: align namespaces to singular (#341)\n\n* chore: bump math to 2.1 instead of 2.0\r\n\r\n* Revert \"chore: bump math to 2.1 instead of 2.0\"\r\n\r\nThis reverts commit 87f704ac5bfeb42c4e3e3b858e7ac463f55d135a.\r\n\r\n* chore: fix some missed imports\r\n\r\n* chore: remove debug define\r\n\r\n* feat: first pass\r\n\r\n* chore: second pass of renaming\r\n\r\n* fix: cpp tests\r\n\r\n* fix: python tests\r\n\r\n* refactor: change coordinatesbroker\r\n\r\n* fix: CoordinatesBroker python bindings\r\n\r\n* Apply suggestions from code review\r\n\r\nself review\r\n\r\n* refactor: coordinatessubsets namespace\r\n\r\n* fix: documentation build\r\n\r\n* feat: align namespaces to singular suggestions (#342)\r\n\r\nfeat: suggestions\r\n\r\n---------\r\n\r\nCo-authored-by: Vishwa Shah <vishwa2710@gmail.com>",
+          "timestamp": "2024-02-13T10:14:00-08:00",
+          "tree_id": "104e41655feebfe5389d1d5bb7c6ef2d40d86f12",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/6b65f18a98bf5697f742066c7b2a0dc649485a5d"
+        },
+        "date": 1707850036236,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 557354938.8999994,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 557314319.9999999 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 803662296.0000045,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 803602269.9999996 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 2796567226.7000003,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2796425439.9999995 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 2791574899.600016,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2791468739.999999 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 2792601919.9000053,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2792409050.0000005 ns\nthreads: 1"
           }
         ]
       }
