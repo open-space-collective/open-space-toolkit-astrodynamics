366e29af20d1cdfdb9001043697bd2c8ee790eeb
github-action-benchmark <github@users.noreply.github.com>
2024-02-13 00:10:55 +0000

    add Benchmark (googlecpp) benchmark result for 75407430ca219ec4de370983ac73d0a05bdeaabe

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707762922771,
+  "lastUpdate": 1707783055069,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -108,6 +108,60 @@ window.BENCHMARK_DATA = {
             "value": 2793297819.6999955,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 2793197089.9999995 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kyle.ray.cochran@gmail.com",
+            "name": "kyle-cochran",
+            "username": "kyle-cochran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "75407430ca219ec4de370983ac73d0a05bdeaabe",
+          "message": "chore: bump Core, IO, Math, and Physics (#336)\n\n* chore: bump Core, IO, and Math\r\n\r\n* chore: bump physics and align namespaces\r\n\r\n* chore: fix divergence from rebase\r\n\r\n* chore: bump math to 2.1 instead of 2.0\r\n\r\n* Revert \"chore: bump math to 2.1 instead of 2.0\"\r\n\r\nThis reverts commit 87f704ac5bfeb42c4e3e3b858e7ac463f55d135a.\r\n\r\n* chore: fix some missed imports\r\n\r\n* chore: remove debug define",
+          "timestamp": "2024-02-12T15:37:19-08:00",
+          "tree_id": "6fa346d340601248ffdd66b40afbc53f539d9c8f",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/75407430ca219ec4de370983ac73d0a05bdeaabe"
+        },
+        "date": 1707783053556,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 557948861.500006,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 557901210 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 801004604.7000031,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 800954020.0000004 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 2791659212.5000024,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2791326689.999999 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 2788070107.0999975,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2787902350.000001 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 2789025667.4,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2788813519.999999 ns\nthreads: 1"
           }
         ]
       }
