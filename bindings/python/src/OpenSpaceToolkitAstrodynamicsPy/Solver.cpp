08d50279515843aa0712cbe67144accc4decc488
vishwa shah <vishwa2710@gmail.com>
2024-02-15 00:30:07 +0000

    chore: missed a spot

--- bindings/python/src/OpenSpaceToolkitAstrodynamicsPy/Solver.cpp
+++ bindings/python/src/OpenSpaceToolkitAstrodynamicsPy/Solver.cpp
@@ -6,9 +6,9 @@
 inline void OpenSpaceToolkitAstrodynamicsPy_Solver(pybind11::module& aModule)
 {
     // Create "solver" python submodule
-    auto solvers = aModule.def_submodule("solver");
+    auto solver = aModule.def_submodule("solver");
 
     // Add objects to "solver" submodule
-    OpenSpaceToolkitAstrodynamicsPy_Solver_TemporalConditionSolver(solvers);
-    OpenSpaceToolkitAstrodynamicsPy_Solver_FiniteDifferenceSolver(solvers);
+    OpenSpaceToolkitAstrodynamicsPy_Solver_TemporalConditionSolver(solver);
+    OpenSpaceToolkitAstrodynamicsPy_Solver_FiniteDifferenceSolver(solver);
 }
