db400be4b54850fec8adb2ced575c0c03dd0d053
github-action-benchmark <github@users.noreply.github.com>
2024-03-14 17:34:44 +0000

    add Benchmark (googlecpp) benchmark result for 572b590d3d93efb7cfd9ab122bba212c8d780ebe

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710251390544,
+  "lastUpdate": 1710437683925,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -54,6 +54,60 @@ window.BENCHMARK_DATA = {
             "value": 2789444718.999988,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 2789332111.999998 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "98616558+apaletta3@users.noreply.github.com",
+            "name": "Antoine Paletta",
+            "username": "apaletta3"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "572b590d3d93efb7cfd9ab122bba212c8d780ebe",
+          "message": "fix: maneuver creation error message (#368)",
+          "timestamp": "2024-03-14T17:16:59Z",
+          "tree_id": "77fa4b8e3f9e79ca7a516ae1ff2d591b4190b936",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/572b590d3d93efb7cfd9ab122bba212c8d780ebe"
+        },
+        "date": 1710437681957,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 555144112.7999994,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 555107161.3999999 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 796838347.0999954,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 796793637.3999999 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 2779985741.59999,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2779886941.4000006 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 2781814800.99999,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2781689598.0999956 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 2787370005.600002,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2787261049.1000004 ns\nthreads: 1"
           }
         ]
       }
