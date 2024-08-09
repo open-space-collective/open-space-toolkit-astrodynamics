989f86295e21afed775db56652d67d4388e31a35
Vishwa Shah <vishwa2710@gmail.com>
2024-08-09 10:29:28 -0700

    refactor!: remove unnecessary converters and add 'Z' parsing (#427)
    refactor: remove unnecessary converters and add 'Z' parsing

--- bindings/python/tools/python/ostk/astrodynamics/converters.py
+++ bindings/python/tools/python/ostk/astrodynamics/converters.py
@@ -1,18 +1,10 @@
 # Apache License 2.0
 
-from datetime import datetime, timedelta, timezone
-
-import numpy as np
-
-from ostk.mathematics.geometry.d3.transformation.rotation import Quaternion
+from datetime import datetime, timezone
 
 from ostk.physics.time import Instant
 from ostk.physics.time import Interval
-from ostk.physics.time import Duration
 from ostk.physics.time import Scale
-from ostk.physics.coordinate import Position
-from ostk.physics.coordinate import Velocity
-from ostk.physics.coordinate import Frame
 
 
 def coerce_to_datetime(value: Instant | datetime | str) -> datetime:
@@ -33,6 +25,9 @@ def coerce_to_datetime(value: Instant | datetime | str) -> datetime:
         return value.get_date_time(Scale.UTC).replace(tzinfo=timezone.utc)
 
     if isinstance(value, str):
+        if value.endswith("Z"):
+            value = value.replace("Z", "+00:00")
+
         return datetime.fromisoformat(value)
 
     raise TypeError("Argument must be a datetime, an Instant, or a str.")
@@ -86,13 +81,15 @@ def coerce_to_iso(value: Instant | datetime | str, timespec: str = "microseconds
 
 
 def coerce_to_interval(
-    value: Interval | tuple[Instant, Instant] | tuple[datetime, datetime],
+    value: (
+        Interval | tuple[Instant, Instant] | tuple[datetime, datetime] | tuple[str, str]
+    ),
 ) -> Interval:
     """
     Return Interval from value.
 
     Args:
-        value (Interval | tuple[Instant, Instant] | tuple[datetime, datetime]): A value to coerce.
+        value (Interval | tuple[Instant, Instant] | tuple[datetime, datetime] | tuple[str, str]): A value to coerce.
 
     Returns:
         Interval: The coerced Interval.
@@ -105,81 +102,3 @@ def coerce_to_interval(
         start_instant=coerce_to_instant(value[0]),
         end_instant=coerce_to_instant(value[1]),
     )
-
-
-def coerce_to_duration(value: Duration | timedelta) -> Duration:
-    """
-    Return Duration from value.
-
-    Args:
-        value (Duration | datetime): A value to coerce.
-
-    Returns:
-        Duration: The coerced Duration.
-    """
-
-    if isinstance(value, Duration):
-        return value
-
-    return Duration.seconds(value.total_seconds())
-
-
-def coerce_to_position(
-    value: Position | tuple[float, float, float] | list[float] | np.ndarray,
-    frame: Frame,
-) -> Position:
-    """
-    Return Position from value.
-
-    Args:
-        value (Position | tuple[float, float, float] | list[float] | np.ndarray): A value to coerce.
-        frame (Frame): A reference frame.
-
-    Returns:
-        Position: The coerced Position.
-    """
-
-    if isinstance(value, Position):
-        return value
-
-    return Position.meters(value, frame)
-
-
-def coerce_to_velocity(
-    value: Velocity | tuple[float, float, float] | list[float] | np.ndarray,
-    frame: Frame,
-) -> Velocity:
-    """
-    Return Velocity from value.
-
-    Args:
-        value (Velocity | tuple[float, float, float] | list[float] | np.ndarray): A value to coerce.
-        frame (Frame): A reference frame.
-
-    Returns:
-        Velocity: The coerced Velocity.
-    """
-
-    if isinstance(value, Velocity):
-        return value
-
-    return Velocity.meters_per_second(value, frame)
-
-
-def coerce_to_quaternion(
-    value: Quaternion | tuple[float, float, float, float] | list[float] | np.ndarray
-) -> Quaternion:
-    """
-    Return quaternion from value.
-
-    Args:
-        value (Quaternion | tuple[float, float, float, float] | list[float] | np.ndarray): A value to coerce.
-
-    Returns:
-        Quaternion: The coerced Quaternion.
-    """
-
-    if isinstance(value, Quaternion):
-        return value
-
-    return Quaternion(vector=value, format=Quaternion.Format.XYZS)
