5a924dfd4ba5f982831096f085f21d7e9746927e
github-action-benchmark <github@users.noreply.github.com>
2024-10-24 16:15:43 +0000

    add Benchmark (googlecpp) benchmark result for 88e4315cf50313a33c2401d4edc452052bc07c38

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729636219603,
+  "lastUpdate": 1729786542954,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -108,6 +108,60 @@ window.BENCHMARK_DATA = {
             "value": 3174361316.100004,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 3174235271.5000024 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vishwa2710@gmail.com",
+            "name": "Vishwa Shah",
+            "username": "vishwa2710"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "88e4315cf50313a33c2401d4edc452052bc07c38",
+          "message": "docs: fix documentation for State.in_frame (#452)",
+          "timestamp": "2024-10-24T08:54:39-07:00",
+          "tree_id": "e71bd34c8f713339e200c4f3f0809501e9d89f0f",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/88e4315cf50313a33c2401d4edc452052bc07c38"
+        },
+        "date": 1729786540099,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 589921136.9000056,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 589884716.3999999 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 880435605.000008,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 880385354.9000002 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 3230406983.0000005,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3230018894 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 3229628172.7999997,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3229421168.999997 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 3229017978.2000055,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3228812764.0999975 ns\nthreads: 1"
           }
         ]
       }
