e329bd39430720df75cd6f55b104335d09f389ed
vishwa shah <vishwa2710@gmail.com>
2024-11-12 07:54:07 +0000

    feat: cleaner interface, wip

--- include/OpenSpaceToolkit/Astrodynamics/Access/Generator.hpp
+++ include/OpenSpaceToolkit/Astrodynamics/Access/Generator.hpp
@@ -58,7 +58,114 @@ using ostk::astrodynamics::trajectory::State;
 #define DEFAULT_STEP Duration::Minutes(1.0)
 #define DEFAULT_TOLERANCE Duration::Microseconds(1.0)
 
-class GroundTargetConfiguration;
+/// @brief Represents the configuration for a ground target, including azimuth, elevation, and range intervals, as well
+/// as position and LLA (Latitude, Longitude, Altitude).
+///
+/// @details This class provides methods to retrieve the trajectory, position, LLA, and intervals for azimuth,
+/// elevation, and range. It also includes a method to get the SEZ (South-East-Zenith) rotation matrix.
+class AccessTarget
+{
+   public:
+    enum class Type
+    {
+        Fixed,
+        Trajectory
+    };
+
+    /// @brief Constructor
+    /// @param aPosition A position
+    // AccessTarget(const Type& aType, const Constraint& constrant, const Position& aPosition);
+
+    /// @brief Get the type
+    ///
+    /// @code{.cpp}
+    ///              AccessTarget accessTarget = { ... } ;
+    ///              AccessTarget::Type type = accessTarget.getType();
+    /// @endcode
+    ///
+    /// @return The type
+    Type getType() const;
+
+    /// @brief Get the constraint
+    ///
+    /// @code{.cpp}
+    ///              AccessTarget accessTarget = { ... } ;
+    ///              Constraint constraint = accessTarget.getConstraint();
+    /// @endcode
+    ///
+    /// @return The constraint
+    Constraint getConstraint() const;
+
+    /// @brief Get the trajectory
+    ///
+    /// @code{.cpp}
+    ///              AccessTarget accessTarget = { ... } ;
+    ///              Trajectory trajectory = accessTarget.getTrajectory();
+    /// @endcode
+    ///
+    /// @return The trajectory
+    Trajectory getTrajectory() const;
+
+    /// @brief Get the position
+    ///
+    /// @code{.cpp}
+    ///              AccessTarget accessTarget = { ... } ;
+    ///              Position position = accessTarget.getPosition();
+    /// @endcode
+    ///
+    /// @return The position
+    Position getPosition() const;
+
+    /// @brief Get the latitude, longitude, and altitude (LLA)
+    ///
+    /// @code{.cpp}
+    ///              AccessTarget accessTarget = { ... } ;
+    ///              LLA lla = accessTarget.getLLA();
+    /// @endcode
+    ///
+    /// @param aCelestialSPtr A celestial body
+    ///
+    /// @return The latitude, longitude, and altitude (LLA)
+    LLA getLLA(const Shared<const Celestial>& aCelestialSPtr) const;
+
+    /// @brief Get the rotation matrix (Matrix3d) from ECEF (Earth-Centered-Earth-Fixed) to SEZ (South-East-Zenith)
+    /// frame
+    ///
+    /// @code{.cpp}
+    ///              AccessTarget accessTarget = { ... } ;
+    ///              Matrix3d sezRotation = accessTarget.computeR_SEZ_ECEF();
+    /// @endcode
+    ///
+    /// @return The SEZ rotation matrix
+    Matrix3d computeR_SEZ_ECEF(const Shared<const Celestial>& aCelestialSPtr) const;
+
+    /// @brief Construct a ground target configuration from an LLA (Latitude, Longitude, Altitude)
+    ///
+    /// @code{.cpp}
+    ///              AccessTarget accessTarget = AccessTarget::LLA(
+    ///                  constraint, lla, aCelestialSPtr
+    ///              );
+    /// @endcode
+    ///
+    /// @param constraint
+    /// @param anLLA
+    /// @param aCelestialSPtr
+    /// @return Ground target configuration
+    static AccessTarget FromLLA(
+        const Constraint& constraint, const LLA& anLLA, const Shared<const Celestial>& aCelestialSPtr
+    );
+
+    static AccessTarget FromPosition(const Constraint& constraint, const Position& aPosition);
+
+    static AccessTarget FromTrajectory(const Constraint& constraint, const Trajectory& aTrajectory);
+
+   private:
+    Type type_;
+    Constraint constraint_;
+    Trajectory trajectory_;
+
+    AccessTarget(const Type& aType, const Constraint& constrant, const Trajectory& aTrajectory);
+};
 
 class Generator
 {
@@ -66,16 +173,9 @@ class Generator
     Generator(
         const Environment& anEnvironment,
         const Duration& aStep = DEFAULT_STEP,
-        const Duration& aTolerance = DEFAULT_TOLERANCE
-    );
-
-    Generator(
-        const Environment& anEnvironment,
-        const std::function<bool(const AER&)>& anAerFilter,
+        const Duration& aTolerance = DEFAULT_TOLERANCE,
         const std::function<bool(const Access&)>& anAccessFilter = {},
-        const std::function<bool(const State&, const State&)>& aStateFilter = {},
-        const Duration& aStep = DEFAULT_STEP,
-        const Duration& aTolerance = DEFAULT_TOLERANCE
+        const std::function<bool(const State&, const State&)>& aStateFilter = {}
     );
 
     bool isDefined() const;
@@ -84,32 +184,27 @@ class Generator
 
     Duration getTolerance() const;
 
-    std::function<bool(const AER&)> getAerFilter() const;
-
     std::function<bool(const Access&)> getAccessFilter() const;
 
     std::function<bool(const State&, const State&)> getStateFilter() const;
 
     std::function<bool(const Instant&)> getConditionFunction(
-        const Trajectory& aFromTrajectory, const Trajectory& aToTrajectory
+        const AccessTarget& anAccessTarget, const Trajectory& aToTrajectory
     ) const;
 
     Array<Access> computeAccesses(
-        const physics::time::Interval& anInterval, const Trajectory& aFromTrajectory, const Trajectory& aToTrajectory
+        const physics::time::Interval& anInterval, const AccessTarget& anAccessTarget, const Trajectory& aToTrajectory
     ) const;
 
-    Array<Array<Access>> computeAccessesWithGroundTargets(
+    Array<Array<Access>> computeAccesses(
         const physics::time::Interval& anInterval,
-        const Array<GroundTargetConfiguration>& someGroundTargetConfigurations,
-        const Trajectory& aToTrajectory,
-        const bool& coarse = false
+        const Array<AccessTarget>& someAccessTargets,
+        const Trajectory& aToTrajectory
     ) const;
 
     void setStep(const Duration& aStep);
 
     void setTolerance(const Duration& aTolerance);
-
-    void setAerFilter(const std::function<bool(const AER&)>& anAerFilter);
 
     void setAccessFilter(const std::function<bool(const Access&)>& anAccessFilter);
 
@@ -117,41 +212,25 @@ class Generator
 
     static Generator Undefined();
 
-    /// @brief Construct an access generator with defined AER ranges
-    ///
-    /// @param anAzimuthRange An azimuth interval [deg]
-    /// @param anElevationRange An elevation interval [deg]
-    /// @param aRangeRange A range interval [m]
-    /// @param anEnvironment An environment
-    /// @return An access generator
-    static Generator AerRanges(
-        const Interval<Real>& anAzimuthRange,
-        const Interval<Real>& anElevationRange,
-        const Interval<Real>& aRangeRange,
-        const Environment& anEnvironment
-    );
-
-    /// @brief Construct an access generator with a defined AER mask
-    ///
-    /// @param anAzimuthElevationMask An azimuth-elevation mask [deg]
-    /// @param aRangeRange A range interval [m]
-    /// @param anEnvironment An environment
-    /// @return An access generator
-    static Generator AerMask(
-        const Map<Real, Real>& anAzimuthElevationMask,
-        const Interval<Real>& aRangeRange,
-        const Environment& anEnvironment
-    );
-
    private:
     Environment environment_;
 
     Duration step_;
     Duration tolerance_;
 
-    std::function<bool(const AER&)> aerFilter_;
     std::function<bool(const Access&)> accessFilter_;
     std::function<bool(const State&, const State&)> stateFilter_;
+
+    Array<Access> computeAccessesForTrajectoryTarget(
+        const physics::time::Interval& anInterval, const AccessTarget& anAccessTarget, const Trajectory& aToTrajectory
+    ) const;
+
+    Array<Array<Access>> computeAccessesForFixedTargets(
+        const physics::time::Interval& anInterval,
+        const Array<AccessTarget>& someAccessTargets,
+        const Trajectory& aToTrajectory,
+        const bool& coarse = false
+    ) const;
 
     Array<Access> generateAccessesFromIntervals(
         const Array<physics::time::Interval>& someIntervals,
@@ -165,7 +244,7 @@ class Generator
         const physics::time::Interval& anAnalysisInterval,
         const Vector3d& fromPositionCoordinate_ITRF,
         const Trajectory& aToTrajectory,
-        const GroundTargetConfiguration& aGroundTargetConfiguration
+        const AccessTarget& anAccessTarget
     ) const;
 
     static Array<physics::time::Interval> ComputeIntervals(const VectorXi& inAccess, const Array<Instant>& instants);
@@ -192,175 +271,6 @@ class Generator
         const Trajectory& aToTrajectory,
         const Shared<const Celestial> anEarthSPtr
     );
-
-    static bool IsAboveMask(
-        const Map<Real, Real>& anAzimuthElevationMask, const Real& anAzimuth_rad, const Real& anElevation_rad
-    );
-
-    static Map<Real, Real> ConvertAzimuthElevationMask(const Map<Real, Real>& anAzimuthElevationMask);
-
-    friend class GroundTargetConfiguration;
-};
-
-/// @brief Represents the configuration for a ground target, including azimuth, elevation, and range intervals, as well
-/// as position and LLA (Latitude, Longitude, Altitude).
-///
-/// @details This class provides methods to retrieve the trajectory, position, LLA, and intervals for azimuth,
-/// elevation, and range. It also includes a method to get the SEZ (South-East-Zenith) rotation matrix.
-class GroundTargetConfiguration
-{
-   public:
-    enum class Type
-    {
-        Fixed,
-        Trajectory
-    };
-
-    /// @brief Constructor
-    /// @param aPosition A position
-    // GroundTargetConfiguration(const Type& aType, const Constraint& constrant, const Position& aPosition);
-
-    /// @brief Get the type
-    ///
-    /// @code{.cpp}
-    ///              GroundTargetConfiguration groundTargetConfiguration = { ... } ;
-    ///              GroundTargetConfiguration::Type type = groundTargetConfiguration.getType();
-    /// @endcode
-    ///
-    /// @return The type
-    Type getType() const;
-
-    /// @brief Get the constraint
-    ///
-    /// @code{.cpp}
-    ///              GroundTargetConfiguration groundTargetConfiguration = { ... } ;
-    ///              Constraint constraint = groundTargetConfiguration.getConstraint();
-    /// @endcode
-    ///
-    /// @return The constraint
-    Constraint getConstraint() const;
-
-    /// @brief Get the trajectory
-    ///
-    /// @code{.cpp}
-    ///              GroundTargetConfiguration groundTargetConfiguration = { ... } ;
-    ///              Trajectory trajectory = groundTargetConfiguration.getTrajectory();
-    /// @endcode
-    ///
-    /// @return The trajectory
-    Trajectory getTrajectory() const;
-
-    /// @brief Get the position
-    ///
-    /// @code{.cpp}
-    ///              GroundTargetConfiguration groundTargetConfiguration = { ... } ;
-    ///              Position position = groundTargetConfiguration.getPosition();
-    /// @endcode
-    ///
-    /// @return The position
-    Position getPosition() const;
-
-    /// @brief Get the latitude, longitude, and altitude (LLA)
-    ///
-    /// @code{.cpp}
-    ///              GroundTargetConfiguration groundTargetConfiguration = { ... } ;
-    ///              LLA lla = groundTargetConfiguration.getLLA();
-    /// @endcode
-    ///
-    /// @param aCelestialSPtr A celestial body
-    ///
-    /// @return The latitude, longitude, and altitude (LLA)
-    LLA getLLA(const Shared<const Celestial>& aCelestialSPtr) const;
-
-    // /// @brief Get the azimuth interval
-    // ///
-    // /// @code{.cpp}
-    // ///              GroundTargetConfiguration groundTargetConfiguration = { ... } ;
-    // ///              Interval<Real> groundTargetConfiguration = generator.getAzimuthInterval();
-    // /// @endcode
-    // ///
-    // /// @return The azimuth interval
-    // Interval<Real> getAzimuthInterval() const;
-
-    // /// @brief Get the elevation interval
-    // ///
-    // /// @code{.cpp}
-    // ///              GroundTargetConfiguration groundTargetConfiguration = { ... } ;
-    // ///              Interval<Real> groundTargetConfiguration = generator.getElevationInterval();
-    // /// @endcode
-    // ///
-    // /// @return The elevation interval
-    // Interval<Real> getElevationInterval() const;
-
-    // /// @brief Get the range interval
-    // ///
-    // /// @code{.cpp}
-    // ///              GroundTargetConfiguration groundTargetConfiguration = { ... } ;
-    // ///              Interval<Real> groundTargetConfiguration = generator.getRangeInterval();
-    // /// @endcode
-    // ///
-    // /// @return The range interval
-    // Interval<Real> getRangeInterval() const;
-
-    // /// @brief Get the azimuth-elevation mask
-    // ///
-    // /// @code{.cpp}
-    // ///              GroundTargetConfiguration groundTargetConfiguration = { ... } ;
-    // ///              Map<Real, Real> azimuthElevationMask = groundTargetConfiguration.getAzimuthElevationMask();
-    // /// @endcode
-    // ///
-    // /// @return The azimuth-elevation mask
-    // Map<Real, Real> getAzimuthElevationMask() const;
-
-    /// @brief Check if the elevation-azimuth are within the mask
-    ///
-    /// @code{.cpp}
-    ///              GroundTargetConfiguration groundTargetConfiguration = { ... } ;
-    ///              bool isAboveMask = groundTargetConfiguration.isAboveMask(elevation, azimuth);
-    /// @endcode
-    ///
-    /// @param anAzimuth An azimuth [deg]
-    /// @param anElevation An elevation [deg]
-    /// @return True if the azimuth-elevation are within the mask, false otherwise
-    // bool isAboveMask(const Real& anAzimuth, const Real& anElevation) const;
-
-    /// @brief Get the rotation matrix (Matrix3d) from ECEF (Earth-Centered-Earth-Fixed) to SEZ (South-East-Zenith)
-    /// frame
-    ///
-    /// @code{.cpp}
-    ///              GroundTargetConfiguration groundTargetConfiguration = { ... } ;
-    ///              Matrix3d sezRotation = groundTargetConfiguration.computeR_SEZ_ECEF();
-    /// @endcode
-    ///
-    /// @return The SEZ rotation matrix
-    Matrix3d computeR_SEZ_ECEF(const Shared<const Celestial>& aCelestialSPtr) const;
-
-    /// @brief Construct a ground target configuration from an LLA (Latitude, Longitude, Altitude)
-    ///
-    /// @code{.cpp}
-    ///              GroundTargetConfiguration groundTargetConfiguration = GroundTargetConfiguration::LLA(
-    ///                  constraint, lla, aCelestialSPtr
-    ///              );
-    /// @endcode
-    ///
-    /// @param constraint
-    /// @param anLLA
-    /// @param aCelestialSPtr
-    /// @return Ground target configuration
-    static GroundTargetConfiguration FromLLA(
-        const Constraint& constraint, const LLA& anLLA, const Shared<const Celestial>& aCelestialSPtr
-    );
-
-    static GroundTargetConfiguration FromPosition(const Constraint& constraint, const Position& aPosition);
-
-    static GroundTargetConfiguration FromTrajectory(const Constraint& constraint, const Trajectory& aTrajectory);
-
-   private:
-    Type type_;
-    Constraint constraint_;
-    Trajectory trajectory_;
-
-    GroundTargetConfiguration(const Type& aType, const Constraint& constrant, const Trajectory& aTrajectory);
 };
 
 class GeneratorContext
@@ -373,7 +283,7 @@ class GeneratorContext
         const Generator& aGenerator
     );
 
-    bool isAccessActive(const Instant& anInstant);
+    bool isAccessActive(const Instant& anInstant, const Constraint& aConstraint);
 
     static Pair<State, State> GetStatesAt(
         const Instant& anInstant, const Trajectory& aFromTrajectory, const Trajectory& aToTrajectory
