87a9f6b4911936f36019412e1a8c4c7ae73664ea
github-action-benchmark <github@users.noreply.github.com>
2024-12-05 23:51:53 +0000

    add Benchmark (googlecpp) benchmark result for fba3e5eed67cb804f04815c5daf720002cbb3581

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733162068274,
+  "lastUpdate": 1733442713388,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -54,6 +54,60 @@ window.BENCHMARK_DATA = {
             "value": 3185987643.0999804,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 3185884707.400001 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vishwa2710@gmail.com",
+            "name": "Vishwa Shah",
+            "username": "vishwa2710"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fba3e5eed67cb804f04815c5daf720002cbb3581",
+          "message": "refactor: remove extra quaternion conversion methods (#469)",
+          "timestamp": "2024-12-05T16:33:55-07:00",
+          "tree_id": "0b9e0290c1c64d7f4488b7dba82763176d2a5f26",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/fba3e5eed67cb804f04815c5daf720002cbb3581"
+        },
+        "date": 1733442710672,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 587148304.2000023,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 587131377.4 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 878405508.3000113,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 878365609.4 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 3188679965.299991,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3188543807.4000006 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 3186477175.800013,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3186183228 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 3188205019.800006,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3188075262.3999996 ns\nthreads: 1"
           }
         ]
       }
