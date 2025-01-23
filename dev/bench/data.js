c334ffdcd17ce31b748be6a0ecf8f01df678ae21
github-action-benchmark <github@users.noreply.github.com>
2025-01-23 23:44:17 +0000

    add Benchmark (googlecpp) benchmark result for ce35abf5dfe8c3cd30da62ca697884b4e0bcbdeb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737503959017,
+  "lastUpdate": 1737675856597,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -864,6 +864,60 @@ window.BENCHMARK_DATA = {
             "value": 3195825870.699997,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 3195724995.0000005 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vishwa2710@gmail.com",
+            "name": "Vishwa Shah",
+            "username": "vishwa2710"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ce35abf5dfe8c3cd30da62ca697884b4e0bcbdeb",
+          "message": "feat!: include states with Maneuver (#432)\n\n* wip:\n\n* wip:\n\n* fix: tests\n\n* feat: address remaining feedback\n\n* fix: python tests\n\n* Apply suggestions from code review\n\n* feat: address last bit of feedback\n\n* chore: style\n\n* Apply suggestions from code review\n\nCo-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>\n\n* feat: use maneuver static var\n\n* fix: tests\n\n---------\n\nCo-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>",
+          "timestamp": "2025-01-23T17:25:44-06:00",
+          "tree_id": "46f17bad0ab4ddbe3d5cdc88bb41099e439d180e",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/ce35abf5dfe8c3cd30da62ca697884b4e0bcbdeb"
+        },
+        "date": 1737675854970,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 536079463.2000079,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 535825766.3000002 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 876922393.500007,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 876900066.3000002 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 3190817446.7000036,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3190762033.1000004 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 3188132100.7999926,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3187796158.5000014 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 3193910276.5999904,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3193823903.1000023 ns\nthreads: 1"
           }
         ]
       }
