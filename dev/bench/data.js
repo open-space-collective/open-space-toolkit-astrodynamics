3adf2cdc655a492df56fadaad9a4b2e43eb8952a
github-action-benchmark <github@users.noreply.github.com>
2025-02-02 22:16:04 +0000

    add Benchmark (googlecpp) benchmark result for 9f6be69e03c6a22e6daa8fb0c7d2af6ea579d474

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738528788770,
+  "lastUpdate": 1738534564380,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -108,6 +108,60 @@ window.BENCHMARK_DATA = {
             "value": 3185934694.2000003,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 3185776499.099997 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vishwa2710@gmail.com",
+            "name": "Vishwa Shah",
+            "username": "vishwa2710"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9f6be69e03c6a22e6daa8fb0c7d2af6ea579d474",
+          "message": "feat: od least squares solver (#501)\n\n* feat: add C++ parts of OD Least Squares Solver\n\n* feat: add python bindings and tests\n\n* feat: address remaining feedback\n\n* feat: address remaining feedback\n\n* chore: style\n\n* feat: update from rebase\n\n* refactor: update LeastSquaresSolver.Analysis interface\n\n* chore: style",
+          "timestamp": "2025-02-02T15:57:21-06:00",
+          "tree_id": "ec82b0f7eb9383aaeb07542bf8859d312d4d22ac",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/9f6be69e03c6a22e6daa8fb0c7d2af6ea579d474"
+        },
+        "date": 1738534562583,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 542093030.799998,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 542059142.3 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 876488522.1999975,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 876421700.0000004 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 3197484284.200004,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3197123662.1000004 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 3203611684.800006,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3203414847.9999986 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 3198796978.100012,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3198584737.0000014 ns\nthreads: 1"
           }
         ]
       }
