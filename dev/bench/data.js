6805e2432aac47b57b63c0a2ba231a99e7f0a7dc
github-action-benchmark <github@users.noreply.github.com>
2025-03-27 13:31:16 +0000

    add Benchmark (googlecpp) benchmark result for 37f2472249e3d8187d628f6611390906631ee24f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742498568631,
+  "lastUpdate": 1743082275919,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -54,6 +54,60 @@ window.BENCHMARK_DATA = {
             "value": 3199999376.700015,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 3199875805.2000017 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "137164378+loft-nicolas-sanchez@users.noreply.github.com",
+            "name": "loft-nicolas-sanchez",
+            "username": "loft-nicolas-sanchez"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "37f2472249e3d8187d628f6611390906631ee24f",
+          "message": "fix: use rgb convention for xyz axis in viewer (#537)",
+          "timestamp": "2025-03-27T14:11:22+01:00",
+          "tree_id": "b3478170dd6b315956b1cb4f54958b24ed8b4e16",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/37f2472249e3d8187d628f6611390906631ee24f"
+        },
+        "date": 1743082273716,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 539072834.5000013,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 539028287.2 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 879289756.2999827,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 879262282.0999997 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 3198076989.40001,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3197821279.700001 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 3196201660.9000044,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3196126845.9999986 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 3197532634.999999,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3197441132.199998 ns\nthreads: 1"
           }
         ]
       }
