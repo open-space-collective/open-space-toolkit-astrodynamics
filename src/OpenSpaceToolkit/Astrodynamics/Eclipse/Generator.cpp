138374b9ccf8eb786540513cce04bde6c753d27a
Antoine Paletta <antoine.paletta@loftorbital.com>
2025-07-28 10:18:51 +0000

    fix: format

--- src/OpenSpaceToolkit/Astrodynamics/Eclipse/Generator.cpp
+++ src/OpenSpaceToolkit/Astrodynamics/Eclipse/Generator.cpp
@@ -130,7 +130,7 @@ Array<Eclipse> Generator::generate(
 
     Array<Eclipse> eclipses = Array<Eclipse>::Empty();
     eclipses.reserve(penumbraAndUmbraIntervals.getSize());
-        
+
     // Process each penumbra and umbra interval
     for (const Interval& penumbraAndUmbraInterval : penumbraAndUmbraIntervals)
     {
@@ -143,7 +143,7 @@ Array<Eclipse> Generator::generate(
         // Create eclipse phases
         Array<EclipsePhase> phases = Array<EclipsePhase>::Empty();
         phases.reserve(penumbraIntervals.getSize() + umbraIntervals.getSize());
-        
+
         // Add penumbra phases
         for (const Interval& penumbraInterval : penumbraIntervals)
         {
