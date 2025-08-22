4fd1022718413bac9d6e9bccd1a9ae106e53200b
github-action-benchmark <github@users.noreply.github.com>
2025-08-22 16:32:03 +0000

    add Benchmark (googlecpp) benchmark result for dd4ad02076c402ccffeaea54028cebfac17c8611

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754937863891,
+  "lastUpdate": 1755880323035,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -54,6 +54,60 @@ window.BENCHMARK_DATA = {
             "value": 3207619978.599996,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 3207398387.9000025 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "35860220+alex-liang3@users.noreply.github.com",
+            "name": "Alex Liang",
+            "username": "alex-liang3"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dd4ad02076c402ccffeaea54028cebfac17c8611",
+          "message": "docs: clarify angle units in VisibilityCriterion (#584)",
+          "timestamp": "2025-08-22T16:02:34Z",
+          "tree_id": "97f7a0be2fbb440cf970efc5b9a58b6b7eca3971",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/dd4ad02076c402ccffeaea54028cebfac17c8611"
+        },
+        "date": 1755880321535,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 924611844.7000015,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 798594955.5 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 897613135.3000141,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 897506365.6999995 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 3195809969.199996,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3195508021.7000012 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 3194919922.1999946,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3194628507.8999987 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 3194478818.700003,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 3194222177.9999995 ns\nthreads: 1"
           }
         ]
       }
