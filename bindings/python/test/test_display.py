32f432240f62f21bbe7d4ad3e089124d7ac1304e
vishwa shah <vishwa2710@gmail.com>
2025-08-28 19:19:46 +0000

    tests: add test for display

--- bindings/python/test/test_display.py
+++ bindings/python/test/test_display.py
@@ -1,5 +1,7 @@
 # Apache License 2.0
 
+import pytest
+
 from ostk.mathematics.object import RealInterval
 
 from ostk.physics import Environment
@@ -22,80 +24,158 @@ from ostk.astrodynamics.trajectory.orbit.model import SGP4
 from ostk.astrodynamics.trajectory.orbit.model.sgp4 import TLE
 from ostk.astrodynamics.access import AccessTarget
 from ostk.astrodynamics.access import VisibilityCriterion
+from ostk.astrodynamics.access import Access
+
+
+@pytest.fixture
+def environment() -> Environment:
+    return Environment.default()
+
+
+@pytest.fixture
+def earth() -> Celestial:
+    return environment.access_celestial_object_with_name("Earth")
+
+
+@pytest.fixture
+def search_interval(earth: Celestial) -> RealInterval:
+    start_instant: Instant = Instant.date_time(
+        DateTime(2023, 1, 3, 0, 0, 0),
+        Scale.UTC,
+    )
+    duration: Duration = Duration.hours(1.0)
+    stop_instant: Instant = start_instant + duration
+    return Interval.closed(start_instant, stop_instant)
+
+
+@pytest.fixture
+def access_plot() -> display.AccessesPlot:
+    return display.AccessesPlot(
+        earth=earth,
+        interval=search_interval,
+        trajectory_step=Duration.minutes(5.0),
+        access_step=Duration.seconds(10.0),
+    )
+
+
+@pytest.fixture
+def satellite_1_trajectory(earth: Celestial) -> Orbit:
+    tle_1 = TLE(
+        "1 55076U 23001BV  23146.17959645  .00004328  00000-0  23719-3 0  9993",
+        "2 55076  97.4793 205.9529 0016244  89.9523 270.3571 15.14609100 21723",
+    )
+    return Orbit(SGP4(tle_1), earth)
+
+
+@pytest.fixture
+def satellite_2_trajectory(earth: Celestial) -> Orbit:
+    tle_2 = TLE(
+        "1 48915U 21059AN  23146.32782040  .00004955  00000-0  24678-3 0  9999",
+        "2 48915  97.5954 279.7041 0010303 354.9434   5.1694 15.18004448105867",
+    )
+    return Orbit(SGP4(tle_2), earth)
+
+
+@pytest.fixture
+def ground_station_lla() -> LLA:
+    return LLA(
+        Angle.degrees(70.0),
+        Angle.degrees(160.0),
+        Length.meters(0.0),
+    )
+
+
+@pytest.fixture
+def access_target(
+    environment: Environment,
+    ground_station_lla: LLA,
+) -> AccessTarget:
+    return AccessTarget.from_position(
+        VisibilityCriterion.from_line_of_sight(environment),
+        Position.from_lla(
+            ground_station_lla,
+            environment.access_celestial_object_with_name("Earth"),
+        ),
+    )
+
+
+@pytest.fixture
+def satellite_1_accesses(
+    satellite_1_trajectory: Orbit,
+    environment: Environment,
+    search_interval: RealInterval,
+    access_target: AccessTarget,
+) -> list[Access]:
+    return AccessGenerator(
+        environment=environment,
+        step=Duration.seconds(10.0),
+        tolerance=Duration.seconds(1.0),
+    ).compute_accesses(
+        interval=search_interval,
+        access_target=access_target,
+        to_trajectory=satellite_1_trajectory,
+    )
 
 
 class TestDisplay:
-    def test_accesses_plot(self):
-        start_instant: Instant = Instant.date_time(
-            DateTime(2023, 1, 3, 0, 0, 0),
-            Scale.UTC,
-        )
-        duration: Duration = Duration.hours(1.0)
-        step: Duration = Duration.seconds(10.0)
-        tolerance: Duration = Duration.seconds(1.0)
+    def test_accesses_plot(
+        self,
+        environment: Environment,
+        earth: Celestial,
+        search_interval: RealInterval,
+        access_target: AccessTarget,
+        ground_station_lla: LLA,
+        satellite_1_trajectory: Orbit,
+        satellite_2_trajectory: Orbit,
+        satellite_1_accesses: list[Access],
+        satellite_2_accesses: list[Access],
+    ):
 
-        ground_station_lla: LLA = LLA(
-            Angle.degrees(70.0),
-            Angle.degrees(160.0),
-            Length.meters(0.0),
-        )
-
-        tle_1 = TLE(
-            "1 55076U 23001BV  23146.17959645  .00004328  00000-0  23719-3 0  9993",
-            "2 55076  97.4793 205.9529 0016244  89.9523 270.3571 15.14609100 21723",
-        )
-
-        tle_2 = TLE(
-            "1 48915U 21059AN  23146.32782040  .00004955  00000-0  24678-3 0  9999",
-            "2 48915  97.5954 279.7041 0010303 354.9434   5.1694 15.18004448105867",
-        )
-
-        environment: Environment = Environment.default()
-        earth: Celestial = environment.access_celestial_object_with_name("Earth")
-
-        visibility_criterion: VisibilityCriterion = (
-            VisibilityCriterion.from_line_of_sight(environment)
-        )
-
-        access_target: AccessTarget = AccessTarget.from_position(
-            visibility_criterion,
-            Position.meters(
-                ground_station_lla.to_cartesian(
-                    earth.get_equatorial_radius(),
-                    earth.get_flattening(),
-                ),
-                Frame.ITRF(),
-            ),
-        )
-
-        stop_instant: Instant = start_instant + duration
-        search_interval: RealInterval = Interval.closed(start_instant, stop_instant)
-
-        orbit_1: Orbit = Orbit(SGP4(tle_1), earth)
-        orbit_2: Orbit = Orbit(SGP4(tle_2), earth)
-
-        generator = AccessGenerator(
-            environment=environment,
-            step=step,
-            tolerance=tolerance,
-        )
-
-        accesses_1 = generator.compute_accesses(
+        accesses_plot = display.AccessesPlot(
+            earth=earth,
             interval=search_interval,
-            access_target=access_target,
-            to_trajectory=orbit_1,
+            trajectory_step=Duration.minutes(5.0),
+            access_step=Duration.seconds(10.0),
         )
 
-        assert len(accesses_1) > 0
-
-        accesses_2 = generator.compute_accesses(
-            interval=search_interval,
-            access_target=access_target,
-            to_trajectory=orbit_2,
+        accesses_plot.add_satellite(
+            trajectory=satellite_1_trajectory,
+            accesses=[],
+            rgb=[180, 0, 0],
         )
 
-        assert len(accesses_2) > 0
+        accesses_plot.add_satellite(
+            trajectory=satellite_2_trajectory,
+            accesses=[],
+            rgb=[0, 0, 180],
+        )
 
+        accesses_plot.add_accesses(
+            trajectory=satellite_1_trajectory,
+            accesses=satellite_1_accesses,
+            rgb=[180, 0, 0],
+        )
+
+        accesses_plot.add_accesses(
+            trajectory=satellite_2_trajectory,
+            accesses=satellite_2_accesses,
+            rgb=[0, 0, 180],
+        )
+
+        accesses_plot.add_ground_station(ground_station_lla)
+
+    def test_accesses_plot_backwards_compatibility(
+        self,
+        environment: Environment,
+        earth: Celestial,
+        search_interval: RealInterval,
+        access_target: AccessTarget,
+        satellite_1_trajectory: Orbit,
+        satellite_2_trajectory: Orbit,
+        satellite_1_accesses: list[Access],
+        satellite_2_accesses: list[Access],
+        ground_station_lla: LLA,
+    ):
         accesses_plot = display.AccessesPlot(
             earth=earth,
             interval=search_interval,
@@ -106,13 +186,13 @@ class TestDisplay:
         )
 
         accesses_plot.add_satellite(
-            trajectory=orbit_1,
-            accesses=accesses_1,
+            trajectory=satellite_1_trajectory,
+            accesses=satellite_1_accesses,
             rgb=[180, 0, 0],
         )
 
         accesses_plot.add_satellite(
-            trajectory=orbit_2,
-            accesses=accesses_2,
+            trajectory=satellite_2_trajectory,
+            accesses=satellite_2_accesses,
             rgb=[0, 0, 180],
         )
