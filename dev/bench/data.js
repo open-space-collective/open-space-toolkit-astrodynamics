2d7fa8f187ea69979a325bdb8c6640885a28f96f
github-action-benchmark <github@users.noreply.github.com>
2026-06-22 21:04:02 +0000

    add Benchmark (googlecpp) benchmark result for 30a39924abfd25d0c08fdcc98c4da9934ffb57f6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781169484641,
+  "lastUpdate": 1782162241820,
   "repoUrl": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics",
   "entries": {
     "Benchmark": [
@@ -84,6 +84,90 @@ window.BENCHMARK_DATA = {
             "value": 9.013418507999972,
             "unit": "s/iter",
             "extra": "iterations: 1\ncpu: 9.012343132000012 s\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "35860220+alex-liang3@users.noreply.github.com",
+            "name": "Alex Liang",
+            "username": "alex-liang3"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "30a39924abfd25d0c08fdcc98c4da9934ffb57f6",
+          "message": "feat: add Propagated to Tabulated method (#687)\n\n* feat: add propagated to tabulated method\n\n* chore: fix type",
+          "timestamp": "2026-06-22T13:32:34-07:00",
+          "tree_id": "0fea0935244b756d8f51dd73f257a887c1d8aa45",
+          "url": "https://github.com/open-space-collective/open-space-toolkit-astrodynamics/commit/30a39924abfd25d0c08fdcc98c4da9934ffb57f6"
+        },
+        "date": 1782162239796,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Access | Ground Station <> TLE/iterations:10",
+            "value": 4673108668.099991,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 4618130181.200001 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | Spherical/iterations:10",
+            "value": 4111900125.7999907,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 4111538259.200001 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1984 {100, 100}/iterations:10",
+            "value": 7239785933.500008,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 7239233481.100001 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM1996 {100, 100}/iterations:10",
+            "value": 7235983541.70001,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 7235387767.299997 ns\nthreads: 1"
+          },
+          {
+            "name": "Propagation | Numerical | EGM2008 {100, 100}/iterations:10",
+            "value": 7244998490.299986,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 7244413494.600008 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_Segment_ConstantThrust_Intrack_550_to_580/iterations:1",
+            "value": 1.223549595999998,
+            "unit": "s/iter",
+            "extra": "iterations: 1\ncpu: 1.2234879919999457 s\nthreads: 1"
+          },
+          {
+            "name": "BM_Segment_QLaw_Analytical_SMA_550_to_580/iterations:1",
+            "value": 5.398135615000115,
+            "unit": "s/iter",
+            "extra": "iterations: 1\ncpu: 5.397560089999956 s\nthreads: 1"
+          },
+          {
+            "name": "BM_Segment_QLaw_FiniteDifference_SMA_550_to_580/iterations:1",
+            "value": 11.002758215000085,
+            "unit": "s/iter",
+            "extra": "iterations: 1\ncpu: 11.001663803999975 s\nthreads: 1"
+          },
+          {
+            "name": "BM_Segment_QLaw_Analytical_Frozen_550_to_580/iterations:1",
+            "value": 8.04454504599994,
+            "unit": "s/iter",
+            "extra": "iterations: 1\ncpu: 8.041622078999978 s\nthreads: 1"
+          },
+          {
+            "name": "BM_Segment_ConstantThrust_Intrack_DutyCycle_550_to_580/iterations:1",
+            "value": 11.40465864700002,
+            "unit": "s/iter",
+            "extra": "iterations: 1\ncpu: 11.403711998000006 s\nthreads: 1"
           }
         ]
       }
